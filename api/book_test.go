5c9c4344d136c93832659b04d3326550c8b08a83
abelgana <32614067+abelgana@users.noreply.github.com>
2017-11-04 18:43:45 -0400

    Change case for the first and the second tests
    Hi,
    
    I just changed the case for the first and second test. My tests are failing. If they are supposed to be lowercase please let me know.

--- api/book_test.go
+++ api/book_test.go
@@ -10,11 +10,11 @@ func TestBookToJSON(t *testing.T) {
 	book := Book{Title: "Cloud Native Go", Author: "M.-L. Reimer", ISBN: "0123456789"}
 	json := book.ToJSON()
 
-	assert.Equal(t, `{"title":"Cloud Native Go","author":"M.-L. Reimer","isbn":"0123456789"}`, string(json), "Book JSON marshalling wrong.")
+	assert.Equal(t, `{"Title":"Cloud Native Go","Author":"M.-L. Reimer","ISBN":"0123456789"}`, string(json), "Book JSON marshalling wrong.")
 }
 
 func TestBookFromJSON(t *testing.T) {
-	json := []byte(`{"title":"Cloud Native Go","author":"M.-L. Reimer","isbn":"0123456789"}`)
+	json := []byte(`{"Title":"Cloud Native Go","Author":"M.-L. Reimer","ISBN":"0123456789"}`)
 	book := FromJSON(json)
 	assert.Equal(t, Book{Title: "Cloud Native Go", Author: "M.-L. Reimer", ISBN: "0123456789"}, book, "Book JSON unmarshalling wrong.")
 }
